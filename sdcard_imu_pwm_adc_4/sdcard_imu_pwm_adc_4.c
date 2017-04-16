6e0dea176b6569da793f684eaa418ccfc7dc8ca5
Steven Giewont <Steven Giewont>
2017-04-16 16:54:17 -0400

    Collect IMU data in a matrix, once matrix is filled write all data to sd card at once. also makes a new file after defined amount of data is written to the sd card

--- sdcard_imu_pwm_adc_4/sdcard_imu_pwm_adc_4.c
+++ sdcard_imu_pwm_adc_4/sdcard_imu_pwm_adc_4.c
@@ -30,10 +30,10 @@ Calendar calendar;                                // Calendar used for RTC
 
 Calendar currTime;
 
-#define CAL_ADC_12T30_L  *(int8_t *)(0x1A1E) // Temperature Sensor Calibration-30 C 2.0V ref
-#define CAL_ADC_12T30_H  *(int8_t *)(0x1A1F)
-#define CAL_ADC_12T85_L  *(int8_t *)(0x1A20) // Temperature Sensor Calibration-85 C 2.0V ref
-#define CAL_ADC_12T85_H  *(int8_t *)(0x1A21)
+//#define CAL_ADC_12T30_L  *(int8_t *)(0x1A1E) // Temperature Sensor Calibration-30 C 2.0V ref
+//#define CAL_ADC_12T30_H  *(int8_t *)(0x1A1F)
+//#define CAL_ADC_12T85_L  *(int8_t *)(0x1A20) // Temperature Sensor Calibration-85 C 2.0V ref
+//#define CAL_ADC_12T85_H  *(int8_t *)(0x1A21)
 
 #if defined(__IAR_SYSTEMS_ICC__)
 #pragma location = 0x9000
@@ -48,9 +48,9 @@ void enterLPM35(void);
 void blinkP1OUT(int blink_number);
 
 
-signed int data_array [500];
-signed int ms_array [500];
-signed int sd_data_matrix[500][500];
+//signed int data_array [500];
+//signed int ms_array [500];
+signed int sd_data_matrix[250][4];
 
 
 //-----------------------------------------------------------------------------
@@ -90,6 +90,7 @@ int main(void) {
     blinkP1OUT(3);
 
 	int log_num = 0;
+	int num_file = 0;
 	char test_data[] = "how are you";
 	char time_sec[8];
 	char curr_ms[8];
@@ -99,9 +100,10 @@ int main(void) {
 	char z_gyro_char[8];
 	char adc_char[8];
 	int current_adc_val;
+	long rtc_ms;
 
 	int row = 0;
-	int col = 0;
+	//int col = 0;
 
 	//start the RTC
 	Init_RTC();
@@ -114,82 +116,89 @@ int main(void) {
 	// setup ADC to read from Header J8, SPEED_IN
 	setupADC();
 
-	//Init_Clock();
+	Init_Clock();
 
-	//setup_IMU_SPI();
-	//write_IMU_SPI(SMPL_PRD, SMPL_PRD_1);
+	while(num_file < 5){
 
-	//SDFindOpenNewFile();
+		while (log_num < 1000) {
+			//initialze the SPI
+			setup_IMU_SPI();
 
-	while (log_num < 1000) {
-		//initialze the SPI
-		setup_IMU_SPI();
+			// grab IMU data
+			z_gyro_data = read_IMU_SPI(ZGYRO);
 
-		// grab IMU data
-		z_gyro_data = read_IMU_SPI(ZGYRO);
+			//store in an array to plot later
+			sd_data_matrix[row][2] = z_gyro_data;
 
-		//store in an array to plot later
-		data_array[row] = z_gyro_data;
-		row++;
-		//convert the zgyro int to a char string
-		itoa(z_gyro_data, z_gyro_char, 10);
+			// if CW rotation is detected, make a PWM that is linearly proportional to the IMU data
+			if(z_gyro_data >= 30){
+				motorSpeed(z_gyro_data);
+				//spin the motor in CW direction
+				motorCW();
+				motorON();
+			}
+
+			// if CCW motion is detected, make a PWM that is linearly proportional to the IMU data
+			else if(z_gyro_data < -30){
+				// invert the magnitude, so a positive value can be sent to PWM
+				z_gyro_data = z_gyro_data * -1;
+				motorSpeed(z_gyro_data);
+
+				// spin motor CCW
+				motorCCW();
+				motorON();
+			}
+
+			else{
+				motorOFF();
+			}
+
+			//capture current ADC value
+			current_adc_val = readADC();
+			sd_data_matrix[row][3] = current_adc_val;
+
+			Init_Clock();
+
+			//get current time in seconds from RTC_C
+			currTime = RTC_C_getCalendarTime(RTC_C_BASE);
+			curr_sec = currTime.Seconds;
+			sd_data_matrix[row][0] = curr_sec;
 
 
-		// if CW rotation is detected, make a PWM that is linearly proportional to the IMU data
-		if(z_gyro_data >= 30){
-			motorSpeed(z_gyro_data);
-			//spin the motor in CW direction
-			motorCW();
-			motorON();
+			rtc_ms =  RTCPS;
+			//templ = (long)rtcps_val * 1000l;
+			rtc_ms = (int)(((long)rtc_ms * 1000l)/32768l);
+			sd_data_matrix[row][1] = rtc_ms;
+
+			log_num++;
+			row++;
+
+			if(row == 250){
+				GPIO_setOutputHighOnPin(GPIO_PORT_P1, GPIO_PIN0);
+				SDFindOpenNewFile();
+
+				for(row = 0; row < 250; row++){
+					itoa(sd_data_matrix[row][0], time_sec, 10);
+					itoa(sd_data_matrix[row][1], curr_ms, 10);
+					itoa(sd_data_matrix[row][2], z_gyro_char, 10);
+					itoa(sd_data_matrix[row][3], adc_char, 10);
+					writeDataSameLine_4(time_sec, curr_ms, z_gyro_char, adc_char);
+				}
+
+				SDCloseSPI();
+				GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
+				row = 0;
+			}
+
+			blinkP1OUT(1);
+			__delay_cycles(10000);
 		}
-
-		// if CCW motion is detected, make a PWM that is linearly proportional to the IMU data
-		else if(z_gyro_data < -30){
-			// invert the magnitude, so a positive value can be sent to PWM
-			z_gyro_data = z_gyro_data * -1;
-			motorSpeed(z_gyro_data);
-
-			// spin motor CCW
-			motorCCW();
-			motorON();
-		}
-
-		else{
-			motorOFF();
-		}
-
-		//capture current ADC value
-		current_adc_val = readADC();
-		itoa(current_adc_val, adc_char, 10);
-//		adc_array[i] = current_adc_val;
-
-		Init_Clock();
-
-		//get current time in seconds from RTC_C
-		currTime = RTC_C_getCalendarTime(RTC_C_BASE);
-		curr_sec = currTime.Seconds;
-		itoa(curr_sec, time_sec, 10);
-
-		//itoa(curr_ms, TB0R, 10);
-		ms_array[row] = TB0R;
-		itoa(ms_array[row], curr_ms, 10);
-
-		//GPIO_setOutputHighOnPin(GPIO_PORT_P7, GPIO_PIN5);
-		// Write the time and gyro data to a single line
-		//writeDataSameLine(time_sec, z_gyro_char, adc_char);
-		//writeDataSameLine_4(time_sec, curr_ms, z_gyro_char, adc_char);
-		writeDataSameLine(curr_ms, z_gyro_char, adc_char);
-
-		//writeData(time_sec);
-		//GPIO_setOutputLowOnPin(GPIO_PORT_P7, GPIO_PIN5);
-
-		log_num++;
-
-		//blink an LED
-		blinkP1OUT(1);
+		log_num = 0;
+		num_file++;
+		 // create new file to save data
+		SDCardNewFile();
 	}
 
-	//SDCloseSPI();
 	GPIO_setOutputHighOnPin(GPIO_PORT_P1, GPIO_PIN0);
 	while(1);
 }
@@ -243,7 +252,7 @@ void Init_GPIO()
  */
 void Init_Clock()
 {
-    // Set DCO frequency to 8 MHz
+    // Set DCO frequency to 16 MHz
     CS_setDCOFreq(CS_DCORSEL_1, CS_DCOFSEL_4);
     //Set external clock frequency to 32.768 KHz
     CS_setExternalClockSource(32768, 0);
@@ -284,9 +293,7 @@ void Init_RTC()
                          RTC_C_TIME_EVENT_INTERRUPT
                          );
 
-    RTC_C_enableInterrupt(RTC_C_BASE,
-                          RTC_C_TIME_EVENT_INTERRUPT
-                          );
+    RTC_C_enableInterrupt(RTC_C_BASE, RTC_C_TIME_EVENT_INTERRUPT);
 
     TB0CTL |= TBSSEL_1 + ID_2;
     //enable interrupt
@@ -338,7 +345,7 @@ void blinkP1OUT(int blink_number){
 __interrupt void TIMER_B0_ISR(void)
 {
 	TB0CCTL1 &= ~(CCIFG);
-	GPIO_toggleOutputOnPin(GPIO_PORT_P7, GPIO_PIN5);
+	//GPIO_toggleOutputOnPin(GPIO_PORT_P7, GPIO_PIN5);
 }
 
 
